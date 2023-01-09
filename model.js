55592886866123013645712409a2bbb8590f5634
priyanshusingh <9415762705ps@gmail.com>
2023-01-09 21:24:03 +0530

    .

---
+++ model.js
@@ -0,0 +1,18 @@
+var employeeData={
+    "employee_id": 123456789,
+    "name": "Ram Kumar",
+    "hire_date": "1/1/2023",
+    "location": "Kiet, GZB",
+    "consultant": false,
+    "random_num": [24,65,12,94]
+};
+
+
+var employeeData={
+    "employee_id": 123345325,
+    "name": "Ram kumar",
+    "hire_date": "1/1/2023",
+    "location": "Kiet, GZB",
+    "consultant": ["123","23"]
+    
+};
